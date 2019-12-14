d10f73285abb095146b9c1f914253a2a6a73d4aa
Aqours <poweroeh@seoultech.ac.kr>
2019-12-14 16:48:49 +0900

    url 수정

--- routes/board.js
+++ routes/board.js
@@ -5,43 +5,107 @@ require('firebase/auth');
 require('firebase/database');
 
 var userName;
-firebase.auth().onAuthStateChanged(function(user){
-  if(user){
-    var getName = firebase.database().ref('users/' + user.uid + '/name');
-    getName.on('value', function(snapshot){
+var uid;
+
+app.get('/plan', function(req, res){
+  res.redirect('/');
+});
+
+app.get('/tripLog', function(req, res){
+  var page = req.query.pageNo;
+  if(!page){
+    page = 1;
+  }
+
+  if(firebase.auth().currentUser){
+    uid = firebase.auth().currentUser.uid;
+  }
+
+  if(uid){
+    var getName = firebase.database().ref('users/' + uid + '/name');
+    getName.once('value').then(function(snapshot){
         userName = snapshot.val();
+        console.log('받아옴 ' + userName);
+        console.log(userName + ' is login');
+        res.render('tripLog', {userName : userName, pageNo : page});
     });
   }else{
-
+    console.log('no login userName ' + userName);
+    res.render('tripLog', {userName : userName, pageNo : page});
   }
 });
 
-app.get('/', function(req, res){
-  res.redirect('./plan');
-});
-
-app.get('/plan', function(req, res){
-  firebase.auth().onAuthStateChanged(function(user){
-      res.render('index', {userName : user});
-    });
-});
-
 app.get('/countryForum', function(req, res){
-  firebase.auth().onAuthStateChanged(function(user){
-      res.render('countryForum', {userName : user});
+  var page = req.query.pageNo;
+  if(!page){
+    page = 1;
+  }
+
+  if(firebase.auth().currentUser){
+    uid = firebase.auth().currentUser.uid;
+  }
+
+  if(uid){
+    var getName = firebase.database().ref('users/' + uid + '/name');
+    getName.once('value').then(function(snapshot){
+        userName = snapshot.val();
+        console.log('받아옴 ' + userName);
+        console.log(userName + ' is login');
+        res.render('countryForum', {userName : userName, pageNo : page});
     });
+  }else{
+    console.log('no login userName ' + userName);
+    res.render('countryForum', {userName : userName, pageNo : page});
+  }
 });
 
 app.get('/freeForum', function(req, res){
-  res.render('countryForum', {userName : userName});
+  var page = req.query.pageNo;
+  if(!page){
+    page = 1;
+  }
+
+  if(firebase.auth().currentUser){
+    uid = firebase.auth().currentUser.uid;
+  }
+
+  if(uid){
+    var getName = firebase.database().ref('users/' + uid + '/name');
+    getName.once('value').then(function(snapshot){
+        userName = snapshot.val();
+        console.log('받아옴 ' + userName);
+        console.log(userName + ' is login');
+        res.render('freeForum', {userName : userName, pageNo : page});
+    });
+  }else{
+    console.log('no login userName ' + userName);
+    res.render('freeForum', {userName : userName, pageNo : page});
+  }
 });
 
-app.get('/forum', function(req, res){
-  res.redirect('./plan');
+app.get('/tripForum', function(req, res){
+  var page = req.query.pageNo;
+  if(!page){
+    page = 1;
+  }
+  
+  if(firebase.auth().currentUser){
+    uid = firebase.auth().currentUser.uid;
+  }
+
+  if(uid){
+    var getName = firebase.database().ref('users/' + uid + '/name');
+    getName.once('value').then(function(snapshot){
+        userName = snapshot.val();
+        console.log('받아옴 ' + userName);
+        console.log(userName + ' is login');
+        res.render('tripForum', {userName : userName, pageNo : page});
+    });
+  }else{
+    console.log('no login userName ' + userName);
+    res.render('tripForum', {userName : userName, pageNo : page});
+  }
 });
 
-app.get('/free', function(req, res){
-  res.redirect('./plan');
-});
 
 module.exports = app;
