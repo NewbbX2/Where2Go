83a0741c9efe80c38a2ea5e7ea2f0a9cf74ae071
Aqours <poweroeh@seoultech.ac.kr>
2019-12-18 12:38:41 +0900

    오류 처리, 실수 수정

--- server.js
+++ server.js
@@ -33,6 +33,14 @@ app.get('/',function(req, res){
 	res.redirect('/board/tripPlan');
 });
 
+app.use((req, res, next) => {
+  res.status(404).send('잘못된 경로');
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('서버 에러');
+});
+
 http.createServer(app).listen(52273, function(){
 	console.log('Server Running at http://127.0.0.1:52273');
 });
